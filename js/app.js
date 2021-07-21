53dbfba9e3f8af16d748adf02ac1fb5538f610e1
koussay183 <ksobos7@gmail.com>
2021-07-21 19:32:23 +0100

    added project

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// hide the preloader on the load of page
+$(()=>{
+    $("#pre").hide();
+    $('.menuToggler').click(()=>{
+        $(".menuToggler").toggleClass("togglerStart");
+        $('.responsiveMenu').toggleClass("active");
+    });
+    $('.innerCarsImageHolder').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '60px'
+      });           
+});
